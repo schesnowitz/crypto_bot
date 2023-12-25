45e5fc3341707ba77d6ac2e94eadea9f123584b1
Stephen Chesnowitz <steve@margoscargo.com>
2023-12-25 18:44:12 -0500

    logging

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
+import tkinter as tk
+import logging
+from binance_futures import write_log
 
 
 
+logger = logging.getLogger()
 
+
+logger.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+formatter = logging.Formatter("%(asctime)s %(levelname)s :: %(message)s")
+
+stream_handler.setFormatter(formatter)
+stream_handler.setLevel(logging.INFO)
+
+file_handler = logging.FileHandler('info.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+
+logger.addHandler(stream_handler)
+logger.addHandler(file_handler)
+
+logger.debug("logger debug")
+logger.info("logger info")
+logger.warning("logger warning")
+logger.error("logger error")
+
+write_log() 
+
+root = tk.Tk()
+root.mainloop()
 
 
 
