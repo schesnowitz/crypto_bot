eb14139499b0ad1650ade4b3c0c9091ea6b9b189
Stephen Chesnowitz <steve@margoscargo.com>
2023-12-25 20:49:10 -0500

    showing pairs

--- binance_futures.py
+++ binance_futures.py
@@ -1,7 +1,22 @@
 import logging
+import requests
+import pprint
+import json
 
 logger = logging.getLogger()
 
 
-def write_log():
-    logger.info("Hey now from Binance futures file")
+
+
+def get_contracts(count=10):
+    response = requests.get("https://testnet.binancefuture.com/fapi/v1/exchangeInfo")
+    print(response.status_code)
+    # pprint.pprint(response.json()['symbols'][0])
+    contracts = []
+    for contract in response.json()['symbols']:
+        # pprint.pprint(contract)
+        contracts.append(contract['pair'])
+        # pprint.pprint(contracts)
+    return contracts[:count]  
+
+# print(get_contracts())
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 import logging
-from binance_futures import write_log
+from binance_futures import get_contracts
 
 
 
@@ -22,33 +22,34 @@ file_handler.setLevel(logging.DEBUG)
 logger.addHandler(stream_handler)
 logger.addHandler(file_handler)
 
-logger.debug("logger debug")
-logger.info("logger info")
-logger.warning("logger warning")
-logger.error("logger error")
-
-write_log() 
-
-root = tk.Tk()
-root.mainloop()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
 
 
 if __name__ == "__main__":
-    pass
+    root = tk.Tk()
+    # fill in the background so there is no whitespace
+    root.configure(bg="gray12")
+
+    binance_contracts = get_contracts(count=50)
+    i = 0
+    j = 0 
+
+    calibri_font = ("Calibri", 10, "normal")
+    for contract in binance_contracts:
+        # label_widget = tk.Label(root, text=contract, borderwidth=1, relief=tk.GROOVE, width=14)
+        label_widget = tk.Label(root, text=contract, bg="gray12", fg="SteelBlue1", width=14, font=calibri_font)
+        # label_widget.pack(side=tk.TOP)
+        label_widget.grid(row=i, column=j, sticky='ew')
+
+        if i == 4:
+            j += 1
+            i = 0
+        else:
+            i += 1
+        
+
+
+
+    root.mainloop()
